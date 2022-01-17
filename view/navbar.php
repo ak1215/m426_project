e113e34927b935f0cf3e93dfd5c36cd4434f33ea
simgeEmir <simge.emir@noseryoung.ch>
2022-01-17 08:52:54 +0100

    seperated navbar

---
+++ view/navbar.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<!-- Navbar-->
+<nav class="navbar navbar-light fixed-top">
+    <div class="container-fluid">
+        <img class="navbar-brand" src="./view/pictures/logo.png" height="50px" width="40px" </img>
+        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar"
+                aria-controls="offcanvasNavbar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar"
+             aria-labelledby="offcanvasNavbarLabel">
+            <div class="offcanvas-header">
+                <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas"
+                        aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body">
+                <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                    <li class="nav-item">
+
+                        <a class="nav-link active" aria-current="page" href="#" style="color: white"> <img
+                                    src="view/pictures/home-icon.png" style="margin-right: 5px" p height="17px">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#" style="color: white">Products</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#" style="color: white">Contact</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#" style="color: white">Cart</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</nav>
+</html>
