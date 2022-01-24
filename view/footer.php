1bef7a6fcf8524a57983419618499e283274eb97
lilrosinante <60463014+lilrosinante@users.noreply.github.com>
2022-01-24 09:51:29 +0100

    updated register branch

---
+++ view/footer.php
@@ -0,0 +1,57 @@
+<!-- Footer -->
+<footer class="footer text-center text-white">
+    <!-- Grid container -->
+    <div class="container p-4">
+        <!-- Section: Links -->
+        <section class="">
+            <!--Grid row-->
+            <div class="row">
+                <!--Grid column-->
+                <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+                    <h5 class="text-uppercase">Aeki</h5>
+
+                    <ul class="list-unstyled mb-0">
+                        <li>
+                            <p class="text-white">We provide the best Furniture service in Switzerland </p>
+                        </li>
+                    </ul>
+                </div>
+                <!--Grid column-->
+
+                <!--Grid column-->
+                <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+
+                    <ul class="list-unstyled mb-0">
+                        <img src="view/pictures/facebook-icon.png" style="height: 30px; margin-right:10px ">
+                        <img src="view/pictures/instagram-icon.png" style="height: 30px; margin-right:10px">
+                        <img src="view/pictures/email-icon.png" style="height: 30px; margin-right:10px">
+                        <img src="view/pictures/twitter-icon.png" style="height: 30px">
+                    </ul>
+                </div>
+                <!--Grid column-->
+
+                <!--Grid column-->
+                <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+                    <h5 class="text-uppercase">Impressum</h5>
+
+                    <ul class="list-unstyled mb-0">
+                        <li>
+                            <p class="text-white">Find our Infos in the Impressum</p>
+                        </li>
+                    </ul>
+                </div>
+                <!--Grid column-->
+            </div>
+            <!--Grid row-->
+        </section>
+        <!-- Section: Links -->
+    </div>
+    <!-- Grid container -->
+
+    <!-- Copyright -->
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+        © 2020 Aeki. All rights reserved.
+    </div>
+    <!-- Copyright -->
+</footer>
+<!-- Footer -->
